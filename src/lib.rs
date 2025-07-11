b3efc4997d180a33ae8b080bff9dfc5a58822502
Gustavo Bender <gustavo.benderr@gmail.com>
2025-07-11 18:21:25 +0200

    add: from string and to string change: change the cells values to u32

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub enum Sign {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BigInt {
     sign: Sign,
-    limbs: Vec<u64>,
+    limbs: Vec<u32>,
 }
 
 impl BigInt {
@@ -24,15 +24,15 @@ impl BigInt {
         }
     }
 
-    pub fn from_i64(n: i64) -> Self {
+    pub fn from_i32(n: i32) -> Self {
         match n {
             0 => Self::new(),
-            n if n < 0 => Self::from_u64((-n) as u64, Sign::Negative),
-            n => Self::from_u64(n as u64, Sign::Positive),
+            n if n < 0 => Self::from_u32((-n) as u32, Sign::Negative),
+            n => Self::from_u32(n as u32, Sign::Positive),
         }
     }
 
-    pub fn from_u64(n: u64, sign: Sign) -> Self {
+    pub fn from_u32(n: u32, sign: Sign) -> Self {
         if sign == Sign::Zero {
             return Self::new();
         }
@@ -53,17 +53,17 @@ impl BigInt {
         }
     }
 
-    fn div_u64(&mut self, divisor: u64) -> u64 {
-        let mut acc: u128 = 0;
+    fn div_u32(&mut self, divisor: u32) -> u32 {
+        let mut acc: u64 = 0;
         for i in self.limbs.iter_mut().rev() {
-            acc <<= 64;
-            acc += (i.clone()) as u128;
-            let rest = acc % (divisor as u128);
-            *i = (acc / divisor as u128) as u64;
+            acc <<= 32;
+            acc += (i.clone()) as u64;
+            let rest = acc % (divisor as u64);
+            *i = (acc / divisor as u64) as u32;
             acc = rest;
         }
         self.normalize();
-        acc as u64
+        acc as u32
     }
 
     fn show(&self) -> String {
@@ -73,11 +73,50 @@ impl BigInt {
         let mut cloned_value = self.clone();
         let mut actual = String::new();
         while cloned_value.sign != Sign::Zero {
-            let rest = cloned_value.div_u64(10);
+            let rest = cloned_value.div_u32(10);
             actual += format!("{}", rest).as_str();
         }
         let actual = actual.chars().rev().collect::<String>();
         actual
+    }
+
+    fn div2_base10(elements: &mut Vec<u8>) -> u8 {
+        let mut rest = 0;
+        for i in elements.iter_mut().rev() {
+            let actual_value = rest * 10 + *i;
+            rest = actual_value % 2;
+            *i = actual_value / 2;
+        }
+        while let Some(&0) = elements.last() {
+            elements.pop();
+        }
+        rest as u8
+    }
+    fn from_str(elements: &str) -> Self {
+        let mut vec_elements = elements
+            .chars()
+            .rev()
+            .map(|x| x.to_digit(10).unwrap() as u8)
+            .collect::<Vec<u8>>();
+        let mut digits: Vec<u32> = Vec::new();
+        let mut binary_rep: Vec<u8> = Vec::new();
+        while !vec_elements.is_empty() {
+            binary_rep.push(Self::div2_base10(&mut vec_elements) as u8);
+        }
+        let mut acc: u32 = 0;
+        for i in 0..binary_rep.len() {
+            acc += (binary_rep[i] as u32) << (i % 32);
+            if i % 32 == 31 {
+                digits.push(acc);
+                acc = 0;
+            }
+        }
+        digits.push(acc);
+
+        Self {
+            sign: Sign::Positive,
+            limbs: digits,
+        }
     }
 }
 
@@ -87,23 +126,22 @@ impl Add for BigInt {
     fn add(self, rhs: Self) -> Self::Output {
         assert_eq!(self.sign, Sign::Positive, "For now only positive integers");
         assert_eq!(rhs.sign, Sign::Positive, "For now only positive integers");
-        let mut rest: u64 = 0;
-        let mut actual_sum: Vec<u64> = vec![];
+        let mut rest: u32 = 0;
+        let mut actual_sum: Vec<u32> = vec![];
         let max_lenght = self.limbs.len().max(rhs.limbs.len());
         for i in 0..max_lenght {
             let first = *self.limbs.get(i).unwrap_or(&0);
             let second = *rhs.limbs.get(i).unwrap_or(&0);
-            let actual_value = (first as u128) + (second as u128) + (rest as u128);
-            let cell_value = actual_value % ((u64::max_value() as u128) + 1);
-            actual_sum.push(cell_value as u64);
-            rest = (actual_value >> 64) as u64;
+            let actual_value = (first as u64) + (second as u64) + (rest as u64);
+            let cell_value = actual_value % ((u32::max_value() as u64) + 1);
+            actual_sum.push(cell_value as u32);
+            rest = (actual_value >> 32) as u32;
         }
         if rest != 0 {
             actual_sum.push(rest);
         }
         Self {
-            sign: Sign::Positive,
-            limbs: actual_sum,
+            sign: Sign::Positive, limbs: actual_sum,
         }
     }
 }
@@ -128,14 +166,24 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut big = BigInt::from_i64(i64::max_value());
-        let mut value: u128 = i64::max_value() as u128;
+        let mut big = BigInt::from_i32(i32::max_value());
+        let mut value: u64 = i32::max_value() as u64;
         for _ in 0..100 {
-            big = big + BigInt::from_i64(i64::max_value());
-            value += i64::max_value() as u128;
+            for _ in 0..100 {
+                big = big + BigInt::from_i32(i32::max_value());
+                value += i32::max_value() as u64;
+            }
             assert_eq!(big.show(), format!("{}", value));
         }
 
-        assert_eq!(0, 0);
+        assert_eq!(big.show(), format!("{}", value));
+    }
+    #[test]
+    fn conversion() {
+        let number = "19238938192839301829380182038012830810928309180192301298381098209831082038182031802380182093819820380182098093810221892389";
+        let big = BigInt::from_str(&number);
+        println!("{}", big.show());
+
+        assert_eq!(big.show(), number);
     }
 }
